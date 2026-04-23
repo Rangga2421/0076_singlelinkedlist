3da41336bbe7ebbdc594a3a02aa38555371fe212
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-04-23 19:30:12 +0700

    membuat class linked list

--- 0076_Singlelinkedlist.cpp
+++ 0076_Singlelinkedlist.cpp
@@ -9,3 +9,22 @@ public:
     Node *next;
 };
 
+class LinkedList
+{
+     Node *START;
+
+
+     public:
+     LinkedList()
+     {
+        START = NULL;
+     }
+
+     void addNode()
+     {
+        int nim;
+        cout << "/nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
